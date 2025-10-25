c801a5d97ea41828ce9455c625d2be45d296ee06
sambhav605 <sambhavregmi999@gmail.com>
2025-10-25 21:49:06 +0545

    completed creating star background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
+import { StarBackground } from "../components/StarBackground"
+import { ThemeToggle } from "../components/ThemeToggle"
 
 export const Home = () =>{
     return( 
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         {/* Theme toggle */ }
-
+        <ThemeToggle/>
         {/*Background Effect */}
+        <StarBackground/>
 
         {/*Navbar */}
 
