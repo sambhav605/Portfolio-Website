359d5d6274352af5f2f54737ecf5eeab4906f79d
sambhav605 <sambhavregmi999@gmail.com>
2025-10-25 18:56:04 +0545

    Setup Completed

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+
+export const Home = () =>{
+    return( 
+    <div className="min-h-screen">
+        {/* Theme toggle */ }
+
+        {/*Background Effect */}
+
+        {/*Navbar */}
+
+        {/* Main content */}
+
+        {/*Footer */}
+
+    </div>)
+}
